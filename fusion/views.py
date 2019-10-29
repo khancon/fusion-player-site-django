f563aa8376c1d759e837db710d8daed6b2a952b4
connor <cea2aj@virginia.edu>
2019-10-29 19:25:54 -0400

    Test direct connection to mysql

--- fusion/views.py
+++ fusion/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import mysql.connector
 
 # Create your views here.
+
+cnx = mysql.connector.connect(user='aak6sk', password='vahZee6n', host='cs4750.cs.virginia.edu', database='aak6sk')
+mycursor = cnx.cursor()
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def songs(request):
+    mycursor.execute("SELECT * FROM Listener")
+    return HttpResponse(mycursor)
