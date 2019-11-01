4f39f05a63b5c84befdb7da0c284fda129783917
connor <cea2aj@virginia.edu>
2019-11-01 15:53:06 -0400

    Added a search results page and added getCursor helper function

--- fusion/urls.py
+++ fusion/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
+from .views import SearchResultsView, AlbumsView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('songs/',views.songs,name='songs'),
-    path('listeners/', views.listeners, name='listeners')
+    path('albums/', AlbumsView.as_view(),name='albums'),
+    path('listeners/', views.listeners, name='listeners'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
